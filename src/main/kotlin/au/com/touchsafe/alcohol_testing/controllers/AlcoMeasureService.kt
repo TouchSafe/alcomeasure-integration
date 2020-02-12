5883effafe1dff78ff87f8dba10276d8ecdc2844
Evan Bennett <evanbennett@users.noreply.github.com>
2020-02-12 16:47:21 +1000

    Scripted commit: 20200212164557

--- src/main/kotlin/au/com/touchsafe/alcohol_testing/controllers/AlcoMeasureService.kt
+++ src/main/kotlin/au/com/touchsafe/alcohol_testing/controllers/AlcoMeasureService.kt
@@ -8,16 +8,16 @@ import io.ktor.http.cio.websocket.send
 import io.ktor.routing.route
 import io.ktor.websocket.webSocket
 
-typealias AccessControlBoardId = Int
+typealias AccessControllerId = Int
 typealias PersonId = Int
 
-// TODO: When an Access Control Board disconnects/is not connected: Send a notification to TouchSafe; Send a notification/email to Client; regarding alcohol testing not being undertaken.
+// TODO: When an Access Controller disconnects/is not connected: Send a notification to TouchSafe; Send a notification/email to Client; regarding alcohol testing not being undertaken.
 @Suppress("MemberVisibilityCanBePrivate")
 open class AlcoMeasureService : com.github.evanbennett.core.controllers.Controller("/services/alcoMeasure/") {
 
 	override val auditLogLocation = Messages.ALCO_MEASURE_SERVICE
 	override val reverseRoutes by lazy { ReverseRoutes(this) }
-	protected val accessControlBoardsConnected = java.util.concurrent.ConcurrentHashMap<AccessControlBoardId, io.ktor.websocket.DefaultWebSocketServerSession>()
+	protected val accessControllersConnected = java.util.concurrent.ConcurrentHashMap<AccessControllerId, io.ktor.websocket.DefaultWebSocketServerSession>()
 
 	override fun routes(route: io.ktor.routing.Route) {
 		route {
@@ -29,10 +29,10 @@ open class AlcoMeasureService : com.github.evanbennett.core.controllers.Controll
 		}
 	}
 
-	suspend fun requestAlcoMeasureTest(accessControlBoardId: AccessControlBoardId, personId: PersonId, firstName: String, surname: String, call: io.ktor.application.ApplicationCall) {
-		val session = accessControlBoardsConnected[accessControlBoardId]
+	suspend fun requestAlcoMeasureTest(accessControllerId: AccessControllerId, personId: PersonId, firstName: String, surname: String, call: io.ktor.application.ApplicationCall) {
+		val session = accessControllersConnected[accessControllerId]
 		if (session == null) {
-			call.logError("AlcoMeasureService.requestAlcoMeasureTest", "AlcoMeasure is not connected: [${accessControlBoardId}] [${personId}]")
+			call.logError("AlcoMeasureService.requestAlcoMeasureTest", "AlcoMeasure is not connected: [${accessControllerId}] [${personId}]")
 			call.sendEmail("TODO", "TouchSafe Notification - AlcoMeasure Offline", "TODO") // TODO
 		} else {
 				session.send("${AlcoMeasure.TEST_START}$personId;$firstName;$surname")
@@ -40,7 +40,7 @@ open class AlcoMeasureService : com.github.evanbennett.core.controllers.Controll
 	}
 
 	suspend fun io.ktor.websocket.DefaultWebSocketServerSession.connect() {
-		var accessControlBoardId: AccessControlBoardId? = null
+		var accessControllerId: AccessControllerId? = null
 
 		try {
 			for (frame in incoming) {
@@ -48,18 +48,18 @@ open class AlcoMeasureService : com.github.evanbennett.core.controllers.Controll
 					is io.ktor.http.cio.websocket.Frame.Text -> {
 						val message = frame.readText()
 						when {
-							message.startsWith(au.com.touchsafe.access_control_library.AccessControlBoard.UNIQUE_IDENTIFIER_START) -> {
-								val accessControlBoardFactory: au.com.touchsafe.access_control_common.models.generated.AccessControlBoardFactory by com.github.evanbennett.core.ServiceLocator.lazyGet()
-								val accessControlBoardFactoryVersion: au.com.touchsafe.access_control_common.models.generated.AccessControlBoardVersionFactory by com.github.evanbennett.core.ServiceLocator.lazyGet()
-								accessControlBoardId = accessControlBoardFactory.loadAccessControlBoardId(message, call)
-								accessControlBoardsConnected[accessControlBoardId] = this
-								val accessControlBoardVersion = accessControlBoardFactoryVersion.loadReferable(accessControlBoardId, call)
-								val alcoMeasureSerial = accessControlBoardVersion.alcoMeasureSerial.value?.integer ?: throw RuntimeException("Referable Access Control Board Version found with `accessControlBoardId` but it does not have an `alcoMeasureSerial`: [$accessControlBoardId]")
+							message.startsWith(au.com.touchsafe.access_control_library.AccessController.UNIQUE_IDENTIFIER_START) -> {
+								val accessControllerFactory: au.com.touchsafe.access_control_common.models.generated.AccessControllerFactory by com.github.evanbennett.core.ServiceLocator.lazyGet()
+								val accessControllerFactoryVersion: au.com.touchsafe.access_control_common.models.generated.AccessControllerVersionFactory by com.github.evanbennett.core.ServiceLocator.lazyGet()
+								accessControllerId = accessControllerFactory.loadAccessControllerId(message, call)
+								accessControllersConnected[accessControllerId] = this
+								val accessControllerVersion = accessControllerFactoryVersion.loadReferable(accessControllerId, call)
+								val alcoMeasureSerial = accessControllerVersion.alcoMeasureSerial.value?.integer ?: throw RuntimeException("Referable Access Controller Version found with `accessControllerId` but it does not have an `alcoMeasureSerial`: [$accessControllerId]")
 								send(AlcoMeasure.SERIAL_NUMBER_START + alcoMeasureSerial)
 							}
 							message.startsWith(AlcoMeasure.RESULT_START) -> {
-								if (accessControlBoardId == null) throw RuntimeException("Trying to store a result without initialising the connection!!!")
-								storeResult(message, accessControlBoardId, call)
+								if (accessControllerId == null) throw RuntimeException("Trying to store a result without initialising the connection!!!")
+								storeResult(message, accessControllerId, call)
 							}
 							else -> call.logError("AlcoMeasureService.connect", "Unrecognised Text Frame: [${message}]")
 						}
@@ -76,22 +76,22 @@ open class AlcoMeasureService : com.github.evanbennett.core.controllers.Controll
 		}
 
 		// Connection closed:
-		if (accessControlBoardId != null) accessControlBoardsConnected.remove(accessControlBoardId)
+		if (accessControllerId != null) accessControllersConnected.remove(accessControllerId)
 	}
 
-	protected suspend fun storeResult(message: String, _accessControlBoardId: AccessControlBoardId, call: io.ktor.application.ApplicationCall): Int {
+	protected suspend fun storeResult(message: String, _accessControllerId: AccessControllerId, call: io.ktor.application.ApplicationCall): Int {
 		val messageParts = message.split(';')
 		if (messageParts.size != 6) throw RuntimeException("Invalid store result frame: [$message]")
 		val alcoMeasureResultFactory: au.com.touchsafe.alcohol_testing.models.generated.AlcoMeasureResultFactory by com.github.evanbennett.core.ServiceLocator.lazyGet()
 		val alcoMeasureResultId = alcoMeasureResultFactory.COLUMNS.ALCO_MEASURE_RESULT_ID.field()
-		val accessControlBoardId = alcoMeasureResultFactory.COLUMNS.ACCESS_CONTROL_BOARD_ID.field(alcoMeasureResultFactory.COLUMNS.ACCESS_CONTROL_BOARD_ID.DATA_TYPE_SINGLETON(_accessControlBoardId.toString()))
+		val accessControllerId = alcoMeasureResultFactory.COLUMNS.ACCESS_CONTROLLER_ID.field(alcoMeasureResultFactory.COLUMNS.ACCESS_CONTROLLER_ID.DATA_TYPE_SINGLETON(_accessControllerId.toString()))
 		val personId = alcoMeasureResultFactory.COLUMNS.PERSON_ID.field(alcoMeasureResultFactory.COLUMNS.PERSON_ID.DATA_TYPE_SINGLETON(messageParts[1]))
 		val result = alcoMeasureResultFactory.COLUMNS.RESULT.field(alcoMeasureResultFactory.COLUMNS.RESULT.DATA_TYPE_SINGLETON(messageParts[2]))
 		val photo1FileId = alcoMeasureResultFactory.COLUMNS.PHOTO_1_FILE_ID.field(alcoMeasureResultFactory.COLUMNS.PHOTO_1_FILE_ID.DATA_TYPE_SINGLETON(messageParts[3]))
 		val photo2FileId = alcoMeasureResultFactory.COLUMNS.PHOTO_2_FILE_ID.field(alcoMeasureResultFactory.COLUMNS.PHOTO_2_FILE_ID.DATA_TYPE_SINGLETON(messageParts[4]))
 		val photo3FileId = alcoMeasureResultFactory.COLUMNS.PHOTO_3_FILE_ID.field(alcoMeasureResultFactory.COLUMNS.PHOTO_3_FILE_ID.DATA_TYPE_SINGLETON(messageParts[5]))
 		val tested = alcoMeasureResultFactory.COLUMNS.TESTED.field()
-		val alcoMeasureResult = alcoMeasureResultFactory(alcoMeasureResultId, accessControlBoardId, personId, result, photo1FileId, photo2FileId, photo3FileId, tested).insert(call)
+		val alcoMeasureResult = alcoMeasureResultFactory(alcoMeasureResultId, accessControllerId, personId, result, photo1FileId, photo2FileId, photo3FileId, tested).insert(call)
 		return alcoMeasureResult.alcoMeasureResultId.value!!.integer
 	}
 
